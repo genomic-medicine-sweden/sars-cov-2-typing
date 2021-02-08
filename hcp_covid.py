48f4a694fbc12dc14a382741279c1bf799ef74fa
Vilma Canfjorden <vilma.canfjorden@gu.se>
2021-02-08 17:06:27 +0100

    upload, download and list files from covid-wgs dir on selected bucket

--- hcp_covid.py
+++ hcp_covid.py
@@ -20,19 +20,24 @@ def files(args):
 
 # Upload files and json to selected bucket on HCP.
 def upload_fastq(args, files_pg, hcpm):
-#    hcpm = HCPManager(args.endpoint, args.aws_access_key_id, args.aws_secret_access_key)
-#    hcpm.attach_bucket(args.bucket)
-
-    # List and upload files provided bu path flag.
+    # List and upload files provided by path flag.
     if args.path:
         for file_pg in files_pg:
-            hcpm.upload_file(file_pg, args.remotepath+"/"+os.path.basename(file_pg))
+            hcpm.upload_file(file_pg, "covid-wgs/"+os.path.basename(file_pg))
             print(f"uploading: {file_pg}")
 
     if args.filepath:
-        # Uploads associated json files.
+        # Uploads single file.
         hcpm.upload_file(f"{args.filepath}",
-                            "covid-wgs"+os.path.basename(args.filepath))
+                            "covid-wgs/"+os.path.basename(args.filepath))
+
+
+def download_fastq(args, hcpm):
+    if args.download:
+        # Downloads file.
+        obj = hcpm.get_object(f"{args.download}")
+        hcpm.download_file(obj, f"{args.output}/"+os.path.basename(args.download))
+
 
 def search(hcpm):
     lst = (hcpm.search_objects("covid-wgs"))
@@ -41,17 +46,17 @@ def search(hcpm):
 
 
 def arg():
-    parser = argparse.ArgumentParser(prog="uploader.py")
+    parser = argparse.ArgumentParser(prog="hci_covid.py")
     requiredNamed = parser.add_argument_group('required arguments')
-    requiredUpload = parser.add_argument_group('additional required arguments for upload')
+    requiredUpload = parser.add_argument_group('additional required arguments for upload or download')
 
-    requiredUpload.add_argument("-ep", "--endpoint",
+    requiredNamed.add_argument("-ep", "--endpoint",
                             help="endpoint url")
-    requiredUpload.add_argument("-aki", "--aws_access_key_id",
+    requiredNamed.add_argument("-aki", "--aws_access_key_id",
                             help="aws access key id")
-    requiredUpload.add_argument("-sak", "--aws_secret_access_key",
+    requiredNamed.add_argument("-sak", "--aws_secret_access_key",
                             help="aws secret access key")
-    requiredUpload.add_argument("-b", "--bucket",
+    requiredNamed.add_argument("-b", "--bucket",
                             help="bucket name")
     requiredUpload.add_argument("-p", "--path",
                             action='store',
@@ -61,6 +66,10 @@ def arg():
     requiredUpload.add_argument("-l", "--listfiles",
                             action="store_true",
                             help="list existing files")
+    requiredUpload.add_argument("-d", "--download",
+                            help="filepath on HCP (key) for file to download")
+    requiredUpload.add_argument("-o", "--output",
+                            help="outputpath for downloaded file")
 
     args = parser.parse_args()
 
@@ -69,16 +78,20 @@ def arg():
 
 def main():
     args = arg()
-    files_pg = files(args)
 
     # Connect to HCP
     hcpm = HCPManager(args.endpoint, args.aws_access_key_id, args.aws_secret_access_key)
     hcpm.attach_bucket(args.bucket)
-    
+
     if args.listfiles:
         search(hcpm)
 
-   upload_fastq(args, files_pg, hcpm)
+    if args.path or args.filepath:
+        files_pg = files(args)
+        upload_fastq(args, files_pg, hcpm)
+
+    if args.download:
+        download_fastq(args, hcpm)
 
 
 if __name__ == "__main__":
